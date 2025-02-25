52286429ac62a7c0b20ff5a61fbf4badc635104c
ssangttuce <sanghoo1023@gmail.com>
2025-02-25 10:43:37 +0900

    Revert "conditional component"
    This reverts commit 3ca6dc1c782f3e66e327a007cae611ca33a5ef6e.

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -2,8 +2,6 @@ import HomePage from '../pages/Homepage'
 import ProfilePage from '../pages/ProfilePage'
 import BoardPage from '../pages/BoardPage'
 import HelloPage from '../pages/HelloPage'
-import JSXPage from '../pages/JSXPage'
-import ConditionalPage from '../pages/ConditionalPage'
 
 // React Routes, Route 사용 시 배열로 관리하기
 const routes = [
@@ -26,16 +24,6 @@ const routes = [
     path: '/hello',
     element: <HelloPage />,
     title: 'hello',
-  },
-  {
-    path: '/jsx',
-    element: <JSXPage />,
-    title: 'jsx문법',
-  },
-  {
-    path: '/conditional',
-    element: <ConditionalPage />,
-    title: '조건부렌더링',
   },
 ]
 
