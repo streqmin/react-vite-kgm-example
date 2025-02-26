7d194ec4104c59613f11e2b292d02a4cd599ee16
ssangttuce <sanghoo1023@gmail.com>
2025-02-26 11:14:05 +0900

    url parameter

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,5 +1,15 @@
+import { useParams } from 'react-router-dom'
+
 const BoardPage = () => {
-  return <h1 className='text-center mt-5'>보드 페이지 입니다.</h1>
+  const params = useParams()
+  console.log(params)
+
+  return (
+    <h1 className='text-center mt-5'>
+      보드 페이지 입니다.
+      {params.id}번 글입니다
+    </h1>
+  )
 }
 
 export default BoardPage
