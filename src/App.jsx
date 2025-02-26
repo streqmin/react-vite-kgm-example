0b59ea2a1367ea8616503e232a384184456cc589
ssangttuce <sanghoo1023@gmail.com>
2025-02-26 13:03:15 +0900

    createbrowserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import routes from './route/Routes'
 import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Header />
+//         <Routes>
+//           {routes.map((route, index) => {
+//             return <Route key={route.path || index} path={route.path} element={route.element}></Route>
+//           })}
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {routes.map((route, index) => {
-            return <Route key={route.path || index} path={route.path} element={route.element}></Route>
-          })}
-        </Routes>
-      </BrowserRouter>
+      <h1>App Header</h1>
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import App2 from './App2.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './route/Router.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <App2 />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//     <App2 />
+//   </StrictMode>,
+// )
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
